4300e802d31cd3b7e4a968d8bfc9a691e0d5fa63
timaevV <timaevvladislav@gmail.com>
2023-11-16 00:50:02 +0600

    feat:deploy contract

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,5 +2,23 @@ const assert = require("assert")
 
 const { Web3 } = require("web3")
 const ganache = require("ganache")
+const {interface, bytecode} = require("../compile")
 
 const web3 = new Web3(ganache.provider())
+
+let accounts
+let contract
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
+    contract = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ["Hello Vlad"]})
+        .send({from: accounts[0], gas: "1000000"})
+})
+
+
+describe("Inbox", () => {
+    it("accounts", () => {
+      console.log(contract)
+    })
+})
