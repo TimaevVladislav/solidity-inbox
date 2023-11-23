465636a5e7388c86a9a9061b8452bf36840c1ff9
timaevV <timaevvladislav@gmail.com>
2023-11-24 00:56:11 +0600

    feat:hdwallet provider and deploy

---
+++ deploy.js
@@ -0,0 +1,15 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const Web3 = require("web3")
+const { interface, bytecode } = require("./compile")
+
+const provider = new HDWalletProvider(
+    "Our Web3 API Key works across several networks, use it on one or use it on all",
+    "https://goerli.infura.io/v3/ea71abb3090a4aae894cce974163dab9"
+)
+
+const web3 = new Web3(provider)
+const deploy = async () => {
+   const accounts = await web3.eth.getAccounts()
+}
+
+deploy()
